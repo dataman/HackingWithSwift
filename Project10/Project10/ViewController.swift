954c24b1d8e1e7ea0fcbad27214269554796587e
Charley Jones <me@crjones.com>
2015-09-28 09:28:25 -0700

    Project 10

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Charley Jones on 9/27/15.
+//  Copyright © 2015 Charley Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
+        return cell
+    }
+}
+
